3cac93b0bbd4d3e8356ba777d9594a99b9baf037
Virtual-Machine <darrelm@tech-asteroid.com>
2016-10-21 00:18:57 -0400

    Implement json configuration loading and socket server

--- server.go
+++ server.go
@@ -1,12 +1,35 @@
 package main
 
 import (
-    "fmt"
+    "log"
     "net/http"
+    "encoding/json"
+    "os"
+
+    "./socket"
 )
 
+type Configuration struct {
+    Mode    string
+}
+
 func main() {
+	log.SetFlags(log.Lshortfile)
+	
+	file, _ := os.Open("settings.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+	  log.Fatal("Error:", err)
+	}
+
+	hub := socket.NewHub(configuration.Mode)
+	go hub.Run()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		socket.ServeWs(hub, w, r)
+	})
+
 	http.Handle("/", http.FileServer(http.Dir("./client/")))
-    http.ListenAndServe(":8090", nil)
-    fmt.Println("HTTP Server listening on localhost:8090")
+    log.Fatal(http.ListenAndServe(":8090", nil))
 }
