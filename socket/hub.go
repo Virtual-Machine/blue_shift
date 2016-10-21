b61ebeb9b33a9ab1b78235d581162cc9398d5dd3
Virtual-Machine <darrelm@tech-asteroid.com>
2016-10-21 00:17:03 -0400

    Add basic server side socket code to allow clients to make requests and server to selectively broadcast.

---
+++ socket/hub.go
@@ -0,0 +1,81 @@
+package socket
+
+import "log"
+
+type Hub struct {
+	clients map[*Client]bool
+	broadcast chan []byte
+	request chan []byte
+	register chan *Client
+	unregister chan *Client
+	mode string
+}
+
+func NewHub(modeStr string) *Hub {
+	return &Hub{
+		broadcast:  make(chan []byte),
+		request:  	make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+		clients:    make(map[*Client]bool),
+		mode: 		modeStr,
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.connect(client)
+		case client := <-h.unregister:
+			h.disconnect(client)
+		case request := <-h.request:
+			h.intakeRequest(request)
+		case message := <-h.broadcast:
+			h.sendBroadcast(message)
+		}
+	}
+}
+
+func (h *Hub) connect(client *Client){
+	if h.mode == "Debug" {
+		log.Println("Connecting socket")
+	}
+	h.clients[client] = true
+}
+
+func (h *Hub) disconnect(client *Client){
+	if h.mode == "Debug" {
+		log.Println("Disconnecting socket")
+	}
+	if _, ok := h.clients[client]; ok {
+		delete(h.clients, client)
+		close(client.send)
+	}
+}
+
+func (h *Hub) intakeRequest(request []byte){
+	if h.mode == "Debug" {
+		log.Println("Got request: " + string(request))
+	}
+	if string(request) == "BROADCAST" {
+		h.sendBroadcast(request)
+	}
+}
+
+func (h *Hub) sendBroadcast(message []byte){
+	if h.mode == "Debug" {
+		log.Println("Broadcasting: " + string(message))
+	}
+	for client := range h.clients {
+		select {
+		case client.send <- message:
+		default:
+			if h.mode == "Debug" {
+				log.Println("Default Condition -- No message into client.send -- see line 54 hub.go")
+			}
+			close(client.send)
+			delete(h.clients, client)
+		}
+	}
+}
