55c481bd367ca3ade67e816cde14e7d9552d3dba
Virtual-Machine <darrelm@tech-asteroid.com>
2016-10-21 20:27:19 -0400

    Refactor main function

--- server.go
+++ server.go
@@ -35,13 +35,7 @@ var data UserList
 func main() {
 	log.SetFlags(log.Lshortfile)
 	
-	file, _ := os.Open("settings.json")
-	decoder := json.NewDecoder(file)
-	conf := Configuration{}
-	err := decoder.Decode(&conf)
-	if err != nil {
-	  log.Fatal("Error:", err)
-	}
+	conf := decodeConfiguration()
 
 	hub := socket.NewHub(conf.Mode)
 	go hub.Run()
@@ -61,21 +55,12 @@ func main() {
 		for i := range data.List {
 			if data.List[i].Name == u.Name {
 				if data.List[i].Password != u.Password {
-					var err LoginResponse
-					err.Type = "Error"
-					err.Message = "Password is not correct for this user account"
-					json.NewEncoder(w).Encode(err)
+					sendErrorResponse(w)
 					return
 				}
 			}
 		}
-		data.List = append(data.List, u)
-        var res LoginResponse
-        res.Type = "Success"
-        res.Message = "Login successful"
-        res.Name = u.Name
-        res.Password = u.Password
-        json.NewEncoder(w).Encode(res)
+		sendSuccessResponse(w, u)
     })
 
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
@@ -84,4 +69,32 @@ func main() {
 
 	http.Handle("/", http.FileServer(http.Dir("./client/")))
     log.Fatal(http.ListenAndServe(conf.Port, nil))
+}
+
+func decodeConfiguration() Configuration {
+	file, _ := os.Open("settings.json")
+	decoder := json.NewDecoder(file)
+	conf := Configuration{}
+	err := decoder.Decode(&conf)
+	if err != nil {
+	  log.Fatal("Error:", err)
+	}
+	return conf
+}
+
+func sendErrorResponse(w http.ResponseWriter){
+	var err LoginResponse
+	err.Type = "Error"
+	err.Message = "Password is not correct for this user account"
+	json.NewEncoder(w).Encode(err)
+}
+
+func sendSuccessResponse(w http.ResponseWriter, u User){
+	data.List = append(data.List, u)
+    var res LoginResponse
+    res.Type = "Success"
+    res.Message = "Login successful"
+    res.Name = u.Name
+    res.Password = u.Password
+    json.NewEncoder(w).Encode(res)
 }
