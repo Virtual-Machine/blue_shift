7a71eae83899bfb8e91ad42f54f1bdd9eff3132b
Virtual-Machine <darrelm@tech-asteroid.com>
2016-11-01 00:37:08 -0400

    Client side check to prevent submittal of blank username or password

--- client/login.js
+++ client/login.js
@@ -1,24 +1,28 @@
 $loginButton.addEventListener('click', function(){
 	var xhr = new XMLHttpRequest()
 	var submitName = $userName.value
-	xhr.open('PUT', '/login')
-	xhr.setRequestHeader('Content-Type', 'application/json')
-	xhr.onload = function() {
-		if (xhr.status === 200) {
-			var serverResponse = JSON.parse(xhr.responseText)
-			if(serverResponse.Type == "Success"){
-				// MARKER Client -> Login successful
-				$loginButton.parentNode.style.display = 'none'
-				window.activeClient = submitName
-				window.canvas.setBindings()
-				establishSocketConnection(serverResponse.Message)
-			} else {
-				$warnText.textContent = serverResponse.Message
+	if (submitName.trim() != "" && $userPassword.value.trim() != ""){
+		xhr.open('PUT', '/login')
+		xhr.setRequestHeader('Content-Type', 'application/json')
+		xhr.onload = function() {
+			if (xhr.status === 200) {
+				var serverResponse = JSON.parse(xhr.responseText)
+				if(serverResponse.Type == "Success"){
+					// MARKER Client -> Login successful
+					$loginButton.parentNode.style.display = 'none'
+					window.activeClient = submitName
+					window.canvas.setBindings()
+					establishSocketConnection(serverResponse.Message)
+				} else {
+					$warnText.textContent = serverResponse.Message
+				}
 			}
 		}
+		xhr.send(JSON.stringify({
+			name: submitName,
+			password: $userPassword.value
+		}))
+	} else {
+		$warnText.textContent = "Username / Password cannot be blank"
 	}
-	xhr.send(JSON.stringify({
-		name: submitName,
-		password: $userPassword.value
-	}))
 })
